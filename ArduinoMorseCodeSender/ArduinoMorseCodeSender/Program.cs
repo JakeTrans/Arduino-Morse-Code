a446d5226c7ca1fb373e59e0cb9c1fcbdad46f74
JakeTrans <Jake_Damner@Yahoo.co.uk>
2020-03-25 16:46:26 +0000

    ports added, and inital code

--- ArduinoMorseCodeSender/ArduinoMorseCodeSender/Program.cs
+++ ArduinoMorseCodeSender/ArduinoMorseCodeSender/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.IO.Ports;
 
 namespace ArduinoMorseCodeSender
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static SerialPort _serialPort;
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM3";
+            _serialPort.BaudRate = 9600;
+
+            _serialPort.Open();
+
+            _serialPort.Write("SOS" + "\n");
+
+            _serialPort.Close();
         }
     }
 }
