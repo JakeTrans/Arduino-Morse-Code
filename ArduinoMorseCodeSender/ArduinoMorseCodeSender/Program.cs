cf7ffd7829615e689d147053d3f06e0994cf53fd
JakeTrans <Jake_Damner@Yahoo.co.uk>
2020-03-26 14:05:46 +0000

    auto port finding

--- ArduinoMorseCodeSender/ArduinoMorseCodeSender/Program.cs
+++ ArduinoMorseCodeSender/ArduinoMorseCodeSender/Program.cs
@@ -10,13 +10,29 @@ namespace ArduinoMorseCodeSender
         private static void Main(string[] args)
         {
             // Console.WriteLine("Hello World!");
+
+            string[] ports = SerialPort.GetPortNames();
+
+            if (ports.Length > 1)
+            {
+                Console.WriteLine("More than one device connected stopping");
+                Console.ReadLine();
+                return;
+            }
+            else if (ports.Length == 0)
+            {
+                Console.WriteLine("No device connected stopping");
+                Console.ReadLine();
+                return;
+            }
             _serialPort = new SerialPort();
-            _serialPort.PortName = "COM3";
+            //_serialPort.PortName = "COM3";
+            _serialPort.PortName = ports[0];
             _serialPort.BaudRate = 9600;
 
             _serialPort.Open();
 
-            _serialPort.Write("SOS" + "\n");
+            _serialPort.Write("this is a test" + "\n");
 
             _serialPort.Close();
         }
