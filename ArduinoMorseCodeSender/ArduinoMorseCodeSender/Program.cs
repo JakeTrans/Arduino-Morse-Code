93409cba682ff80c10b3188a8c85a50f1764ea83
JakeTrans <Jake_Damner@Yahoo.co.uk>
2020-08-09 12:54:33 +0100

    .net core Console Added

---
+++ ArduinoMorseCodeSender/ArduinoMorseCodeSender/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace ArduinoMorseCodeSender
+{
+    internal class Program
+    {
+        private static SerialPort _serialPort;
+
+        private static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM3";
+            _serialPort.BaudRate = 9600;
+
+            _serialPort.Open();
+
+            _serialPort.Write("SOS" + "\n");
+
+            _serialPort.Close();
+        }
+    }
+}
