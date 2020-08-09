031630caaecbb6ed403acdb315381dbfa31b61b3
JakeTrans <Jake_Damner@Yahoo.co.uk>
2020-08-09 13:31:19 +0100

    auto detect com port

--- ArduinoMorseCodeSender/ArduinoMorseCodeSender/Program.cs
+++ ArduinoMorseCodeSender/ArduinoMorseCodeSender/Program.cs
@@ -1,27 +1,52 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Linq;
 
 namespace ArduinoMorseCodeSender
 {
     internal class Program
     {
-        private static SerialPort _serialPort;
-
         private static void Main(string[] args)
         {
+            SerialPortCommuication SPC = new SerialPortCommuication();
+
             Console.WriteLine("Morse code serial communication Program:");
             Console.WriteLine("Finding Ports");
 
-            string postname = "COM3";
+            string[] ports = SPC.AutodetectArdiuno();
 
-            SerialPortCommuication SPC = new SerialPortCommuication();
+            if (SPC.Connected == true)
+            {
+                Console.WriteLine("Ardiuno found on " + ports[0]);
+            }
+            else if (ports.Length == 0)
+            {
+                Console.WriteLine("No COM port found");
+                Console.ReadLine();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("multiple COM ports found");
+                foreach (string port in ports)
+                {
+                    Console.WriteLine(port);
+                }
 
-            SPC.SetupSerialPort(postname);
+                while (SPC.Connected == false)
+                {
+                    Console.WriteLine("Please type the port name to use.");
+                    string comport = Console.ReadLine();
+
+                    SPC.SetupSerialPort(comport);
+                }
+            }
+
             Console.WriteLine("Please enter message:");
             string message = Console.ReadLine();
-            _serialPort.Write(message + "\n");
+            SPC.serialPort.Write(message + "\n");
 
-            _serialPort.Close();
+            SPC.serialPort.Close();
         }
     }
 }
--- ArduinoMorseCodeSender/ArduinoMorseCodeSender/SerialPortCommuication.cs
+++ ArduinoMorseCodeSender/ArduinoMorseCodeSender/SerialPortCommuication.cs
@@ -1,25 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO.Ports;
+using System.Linq;
 using System.Text;
 
 namespace ArduinoMorseCodeSender
 {
-    public class SerialPortCommuication
+    public class SerialPortCommuication : IDisposable
     {
+        private bool disposedValue;
+        public bool Connected = false;
+
         public SerialPortCommuication()
         {
-            _serialPort = new SerialPort();
+            serialPort = new SerialPort();
         }
 
-        public SerialPort _serialPort { get; set; }
+        public SerialPort serialPort { get; set; }
 
         public void SetupSerialPort(string postname)
         {
-            _serialPort.PortName = postname;
-            _serialPort.BaudRate = 9600;
+            serialPort.PortName = postname;
+            serialPort.BaudRate = 9600;
 
-            _serialPort.Open();
+            serialPort.Open();
+            Connected = true;
+        }
+
+        public string[] AutodetectArdiuno()
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            if (ports.Count() == 1)
+            {
+                SetupSerialPort(ports[0]);
+            }
+
+            return ports;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    serialPort.Close();
+                    serialPort.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
